fd4af9380689ce2f7f7f73497cebdd32bc325684
CI <ci@test.local>
2026-08-17 16:26:36 +0900

    feat: add simple flank rule

--- app/Sources/App/SharedUI/FlankedOrnamentRule.swift
+++ app/Sources/App/SharedUI/FlankedOrnamentRule.swift
@@ -5,25 +5,29 @@ struct FlankedOrnamentRule<Center: View>: View {
     enum Style {
         case `default`
         case grand
+        case simple
 
-        var resourceName: String {
+        var resourceName: String? {
             switch self {
             case .default: "side-ornament"
             case .grand: "side-ornament-grand"
+            case .simple: nil
             }
         }
 
         var size: CGSize {
             switch self {
             case .default: CGSize(width: 118, height: 18)
-            case .grand: CGSize(width: 94, height: 41)
+            case .grand: CGSize(width: 85, height: 37)
+            case .simple: CGSize(width: 0, height: 1)
             }
         }
 
         var minimumWidth: CGFloat {
             switch self {
             case .default: 40
-            case .grand: 52
+            case .grand: 47
+            case .simple: 24
             }
         }
 
@@ -50,21 +54,33 @@ struct FlankedOrnamentRule<Center: View>: View {
 
     @ViewBuilder
     private func ornament(flippedHorizontally: Bool = false) -> some View {
-        if let image = AppSymbolStyle.resourceImage(
-            name: style.resourceName,
-            fileExtension: "svg",
-            subdirectory: "Ornaments",
-            canvasSize: style.size,
-            tintColor: NSColor(color)
-        ) {
-            Image(nsImage: image)
-                .resizable(capInsets: style.capInsets, resizingMode: .stretch)
-                .frame(minWidth: style.minimumWidth, maxWidth: style.size.width, minHeight: style.size.height, maxHeight: style.size.height)
-                .scaleEffect(x: flippedHorizontally ? -1 : 1, y: 1)
+        switch style {
+        case .simple:
+            Rectangle()
+                .fill(LinearGradient(
+                    colors: flippedHorizontally ? [color, color.opacity(0)] : [color.opacity(0), color],
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ))
+                .frame(minWidth: style.minimumWidth, maxWidth: .infinity, minHeight: style.size.height, maxHeight: style.size.height)
                 .layoutPriority(-1)
-        } else {
-            Color.clear
-                .frame(minWidth: style.minimumWidth, maxWidth: style.size.width, minHeight: style.size.height, maxHeight: style.size.height)
+        case .default, .grand:
+            if let resourceName = style.resourceName, let image = AppSymbolStyle.resourceImage(
+                name: resourceName,
+                fileExtension: "svg",
+                subdirectory: "Ornaments",
+                canvasSize: style.size,
+                tintColor: NSColor(color)
+            ) {
+                Image(nsImage: image)
+                    .resizable(capInsets: style.capInsets, resizingMode: .stretch)
+                    .frame(minWidth: style.minimumWidth, maxWidth: style.size.width, minHeight: style.size.height, maxHeight: style.size.height)
+                    .scaleEffect(x: flippedHorizontally ? -1 : 1, y: 1)
+                    .layoutPriority(-1)
+            } else {
+                Color.clear
+                    .frame(minWidth: style.minimumWidth, maxWidth: style.size.width, minHeight: style.size.height, maxHeight: style.size.height)
+            }
         }
     }
 }
