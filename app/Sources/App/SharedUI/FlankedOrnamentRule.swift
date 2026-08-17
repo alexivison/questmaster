5a5c2c7cb17dd561db096d58dfa0ea7f89ea6d48
CI <ci@test.local>
2026-08-18 08:40:44 +0900

    feat: add sprig ornament style

--- app/Sources/App/SharedUI/FlankedOrnamentRule.swift
+++ app/Sources/App/SharedUI/FlankedOrnamentRule.swift
@@ -6,12 +6,14 @@ struct FlankedOrnamentRule<Center: View>: View {
         case `default`
         case grand
         case simple
+        case sprig
 
         var resourceName: String? {
             switch self {
             case .default: "side-ornament"
             case .grand: "side-ornament-grand"
             case .simple: nil
+            case .sprig: "side-ornament-2"
             }
         }
 
@@ -20,6 +22,7 @@ struct FlankedOrnamentRule<Center: View>: View {
             case .default: CGSize(width: 118, height: 18)
             case .grand: CGSize(width: 85, height: 37)
             case .simple: CGSize(width: 0, height: 1)
+            case .sprig: CGSize(width: 111, height: 23)
             }
         }
 
@@ -28,6 +31,7 @@ struct FlankedOrnamentRule<Center: View>: View {
             case .default: 48
             case .grand: 52
             case .simple: 24
+            case .sprig: 30
             }
         }
 
@@ -65,7 +69,7 @@ struct FlankedOrnamentRule<Center: View>: View {
                 ))
                 .frame(minWidth: style.minimumWidth, maxWidth: .infinity, minHeight: style.size.height, maxHeight: style.size.height)
                 .layoutPriority(-1)
-        case .default, .grand:
+        case .default, .grand, .sprig:
             if let resourceName = style.resourceName, let image = AppSymbolStyle.resourceImage(
                 name: resourceName,
                 fileExtension: "svg",
